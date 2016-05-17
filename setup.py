2b463aa118f6262a37d17d32a2c514a02fbbbe82
csboling <csboling@users.noreply.github.com>
2016-05-17 15:19:37 -0400

    more project information in setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='egor',
+    version='0.0.0',
+    url='https://github.com/egor-elab/egor/',
     packages=find_packages(),
     install_requires=[
         'nameko',
