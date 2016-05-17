35a7eb25414012568c852cc39657b6ee2f529f01
Sam Boling <charles.samuel.boling@gmail.com>
2016-05-17 19:28:19 -0400

    better path joining, cleaned imports, fixed typo

--- egor/service/load/git.py
+++ egor/service/load/git.py
@@ -1,5 +1,3 @@
-import os
-
 import eventlet
 from nameko.standalone.rpc import ClusterRpcProxy
 
@@ -25,10 +23,10 @@ class GitLoader(RpcProxyLazyLoader):
             if bare:
                 reponame = name + '.git'
             else:
-                reponame = os.path.join(name, '.git')
+                reponame = '/'.join(name, '.git')
             proxy.servicehost.prep(
                 name,
-                os.path.join(self.base_path, reponame)
+                '/'.join(self.base_path, reponame)
             )
             self.resolve(name)
-            self._proxies.append(name)
+            self._proxies_active.add(name)
